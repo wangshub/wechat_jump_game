4850994879041856cd01fe6a24125f3c9a97a18f
wo0d <thune-xiaobai@users.noreply.github.com>
2017-12-30 03:17:48 +0800

    python3 compatible

--- wechat_jump_auto.py
+++ wechat_jump_auto.py
@@ -21,7 +21,7 @@ def jump(distance):
     press_time = int(press_time)
     # TODO: 坐标根据截图的 size 来计算
     cmd = 'adb shell input swipe 500 1600 500 1601 ' + str(press_time)
-    print cmd
+    print(cmd)
     os.system(cmd)
 
 
