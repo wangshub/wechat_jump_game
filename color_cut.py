49803be0e0cdf94ba948d54003e2bd9cc79a0fbb
wangsong <wangsong@lejurobot.com>
2017-12-29 22:40:13 +0800

    update readme

--- color_cut.py
+++ color_cut.py
@@ -3,64 +3,30 @@
 import numpy as np
 import argparse
 import cv2
-import scipy.misc
-import matplotlib.pyplot as plt
-from PIL import Image
+image = cv2.imread('2.png')
+color = [
+    ([0, 70, 70], [100, 255, 255])#黄色范围~这个是我自己试验的范围，可根据实际情况自行调整~注意：数值按[b,g,r]排布
+]
 
-raw = np.array(Image.open('1.png'))
-raw_r = np.mat(raw[:, :, 0])
+# 120.109.150 
+man_color = [
+    ([120, 109, 150], [120, 109, 150])
+]
 
-print raw_r.shape
+#如果color中定义了几种颜色区间，都可以分割出来 
+for (lower, upper) in man_color:
+    print lower
+    print upper
 
-
-# plt.imshow(raw_r)
-
-label1=0
-label1val=0
-label1num=0
-label1min=0
-label1max=0
-currentPIXval=0
-
-for i in range(720, 1000):
-    label1=0 
-    label2=0 
-    label3=0 
-    label4=0 
-
-    label1val=0 
-    label2val=0 
-    label3val=0 
-    label4val=0 
-    
-    label1min=0 
-    label1max=0 
-    
-    findmaxflag=1 
-
-    for j in range(1080):
-        cur = raw_r[i,j]
-        if cur>(label1val-15) and cur<(label1val+15):
-            label1=label1+1
-            if(findmaxflag):
-                if(j-label1min>10):
-                    label1max=j
-                    findmaxflag=0;
-                else:
-                    label1min=j
-        if(label1val==0):
-            label1val=cur
-            continue;
-        continue;
-
-    if (abs(label1max-label1min)<1080):
-        if( label1max-label1min <= label1num and label1max-label1min>20 ):
-
-            print 'cor = ', (label1max+label1min)/2, i
-            
-            break
-        label1num=label1max-label1min;
+    # 创建NumPy数组
+    lower = np.array(lower, dtype = "uint8")#颜色下限
+    upper = np.array(upper, dtype = "uint8")#颜色上限
 
 
+    # 根据阈值找到对应颜色
+    mask = cv2.inRange(image, lower, upper)
+    output = cv2.bitwise_and(image, image, mask = mask)
 
-
+    # 展示图片
+    cv2.imshow("images", np.hstack([image, output]))
+    cv2.waitKey(0)
