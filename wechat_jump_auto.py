76c1c416129cb524e62e24ff06a00a5dd81f53ed
Vitaly <vitalyankh@gmail.com>
2017-12-31 02:17:19 +0800

    Update wechat_jump_auto.py

--- wechat_jump_auto.py
+++ wechat_jump_auto.py
@@ -82,7 +82,7 @@ def set_button_position(im):
 
 
 def jump(distance):
-    press_time = distance * press_coefficient
+    press_time = math.pow(distance,0.85) * press_coefficient
     press_time = max(press_time, 200)   # 设置 200 ms 是最小的按压时间
     press_time = int(press_time)
     cmd = 'adb shell input swipe {} {} {} {} {}'.format(swipe_x1, swipe_y1, swipe_x2, swipe_y2, press_time)
