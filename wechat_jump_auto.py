b32416992c5cddce9ca087945ddd687fad05a0d4
wjzhangcsu <wjzhangcsu@126.com>
2017-12-31 17:45:51 +0800

    修改配置文件，只需修改分辨率参数即可完成适配

--- wechat_jump_auto.py
+++ wechat_jump_auto.py
@@ -30,13 +30,11 @@ with open('config.json','r') as f:
     config = json.load(f)
 
 # Magic Number，不设置可能无法正常执行，请根据具体截图从上到下按需设置
-resolution = '540*960'
-under_game_score_y = config['under_game_score_y']     # 截图中刚好低于分数显示区域的 Y 坐标，300 是 1920x1080 的值，2K 屏、全面屏请根据实际情况修改
-press_coefficient = config['press_coefficient']       # 长按的时间系数，请自己根据实际情况调节
-# 也可以根据配置文件自行选择合适的参数
-#press_coefficient = config[resolution]['press_coefficient']       
-piece_base_height_1_2 = config['piece_base_height_1_2']   # 二分之一的棋子底座高度，可能要调节
-piece_body_width = config['piece_body_width']             # 棋子的宽度，比截图中量到的稍微大一点比较安全，可能要调节
+resolution = 'default' # 此处使用默认参数，你应该修改为适配你自己机型的参数,请参考config.json文件
+under_game_score_y = config[resolution]['under_game_score_y']     # 截图中刚好低于分数显示区域的 Y 坐标，300 是 1920x1080 的值，2K 屏、全面屏请根据实际情况修改
+press_coefficient = config[resolution]['press_coefficient']       # 长按的时间系数，请自己根据实际情况调节 
+piece_base_height_1_2 = config[resolution]['piece_base_height_1_2']   # 二分之一的棋子底座高度，可能要调节
+piece_body_width = config[resolution]['piece_body_width']             # 棋子的宽度，比截图中量到的稍微大一点比较安全，可能要调节
 
 swipe_x1, swipe_y1, swipe_x2, swipe_y2 = 320, 410, 320, 410     # 模拟按压的起始点坐标，需要自动重复游戏请设置成“再来一局”的坐标
 
