6adca455ea564e7af5caa7b91d7a05d91b1d6be2
F-loat <chaimaoyuan@foxmail.com>
2018-01-01 15:08:10 +0800

    调整截图函数代码顺序

--- wechat_jump_auto.py
+++ wechat_jump_auto.py
@@ -78,19 +78,20 @@ if not os.path.isdir(screenshot_backup_dir):
 
 def pull_screenshot():
     global screenshot_way
-    if screenshot_way == 0:
-        os.system('adb shell screencap -p /sdcard/autojump.png')
-        os.system('adb pull /sdcard/autojump.png .')
-    elif screenshot_way == 1 or screenshot_way == 2:
+    # 新的方法请根据效率及适用性由高到低排序
+    if screenshot_way == 2 or screenshot_way == 1:
         process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
         screenshot = process.stdout.read()
-        if screenshot_way == 1:
-          binary_screenshot = screenshot.replace(b'\r\r\n', b'\n')
-        else:
+        if screenshot_way == 2:
           binary_screenshot = screenshot.replace(b'\r\n', b'\n')
+        else:
+          binary_screenshot = screenshot.replace(b'\r\r\n', b'\n')
         f = open('autojump.png', 'wb')
         f.write(binary_screenshot)
         f.close()
+    elif screenshot_way == 0:
+        os.system('adb shell screencap -p /sdcard/autojump.png')
+        os.system('adb pull /sdcard/autojump.png .')
 
 def backup_screenshot(ts):
     # 为了方便失败的时候 debug
