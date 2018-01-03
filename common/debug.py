8347f530c1e4e8bbefe7c0e7d1f5472dd1b918c1
clip <binderclip2014@gmail.com>
2018-01-03 18:04:19 +0800

    更新 debug 信息

--- common/debug.py
+++ common/debug.py
@@ -48,18 +48,21 @@ def dump_device_info():
     显示设备信息
     """
     size_str = os.popen('adb shell wm size').read()
-    device_str = os.popen('adb shell getprop ro.product.model').read()
+    device_str = os.popen('adb shell getprop ro.product.device').read()
+    os_version_str = os.popen('adb shell getprop ro.build.version.release').read()
     density_str = os.popen('adb shell wm density').read()
     print("""**********
 Screen: {size}
 Density: {dpi}
-DeviceType: {type}
-OS: {os}
+Device: {device}
+Phone OS: {phone_os}
+Host OS: {host_os}
 Python: {python}
 **********""".format(
         size=size_str.strip(),
-        type=device_str.strip(),
         dpi=density_str.strip(),
-        os=sys.platform,
+        device=device_str.strip(),
+        phone_os=os_version_str.strip(),
+        host_os=sys.platform,
         python=sys.version
     ))
