3d1fd4d7e077fb682d942146afdfe48ddb0f6bfb
BeiTown <beitown@qq.com>
2018-01-03 17:30:27 +0800

    [优化]新增AI模块，机器人自主学习生成跳跃公式
    机器人精确采集跳跃结果并自主学习，使用线性回归方法拟合出最优 [按压时间]-[弹跳距离] 线性公式 Y =kX + b

--- common/debug.py
+++ common/debug.py
@@ -1,35 +1,37 @@
-# -*- coding: utf-8 -*-
-"""
-这儿是debug的代码，当DEBUG_SWITCH开关开启的时候，会将各种信息存在本地，方便检查故障
-"""
+# coding: utf-8
+'''
+默认PEP8的docstring，文件注释写在这里
+'''
 import os
 import sys
 import shutil
+import math
 from PIL import ImageDraw
+from common import ai
 
 screenshot_backup_dir = 'screenshot_backups/'
 
 
 def make_debug_dir(screenshot_backup_dir):
-    """
+    '''
     创建备份文件夹
-    """
+    '''
     if not os.path.isdir(screenshot_backup_dir):
         os.mkdir(screenshot_backup_dir)
 
 
 def backup_screenshot(ts):
-    """
+    '''
     为了方便失败的时候 debug
-    """
+    '''
     make_debug_dir(screenshot_backup_dir)
     shutil.copy('autojump.png', '{}{}.png'.format(screenshot_backup_dir, ts))
 
 
-def save_debug_screenshot(ts, im, piece_x, piece_y, board_x, board_y):
-    """
+def save_debug_screenshot(type, ts, im, piece_x, piece_y, board_x, board_y):
+    '''
     对 debug 图片加上详细的注释
-    """
+    '''
     make_debug_dir(screenshot_backup_dir)
     draw = ImageDraw.Draw(im)
     draw.line((piece_x, piece_y) + (board_x, board_y), fill=2, width=3)
@@ -40,13 +42,26 @@ def save_debug_screenshot(ts, im, piece_x, piece_y, board_x, board_y):
     draw.ellipse((piece_x - 10, piece_y - 10, piece_x + 10, piece_y + 10), fill=(255, 0, 0))
     draw.ellipse((board_x - 10, board_y - 10, board_x + 10, board_y + 10), fill=(0, 0, 255))
     del draw
-    im.save('{}{}_d.png'.format(screenshot_backup_dir, ts))
+    im.save('{}{}_{}.png'.format(screenshot_backup_dir, ts, type))
 
+def computing_error(last_press_time, target_board_x, target_board_y, last_piece_x, last_piece_y, temp_piece_x, temp_piece_y):
+    '''
+    计算跳跃实际误差
+    '''
+    target_distance = math.sqrt((target_board_x - last_piece_x) ** 2 + (target_board_y - last_piece_y) ** 2)  # 上一轮目标跳跃距离
+    actual_distance = math.sqrt((temp_piece_x - last_piece_x) ** 2 + (temp_piece_y - last_piece_y) ** 2)  # 上一轮实际跳跃距离
+    jump_error_value = math.sqrt((target_board_x - temp_piece_x) ** 2 + (target_board_y - temp_piece_y) ** 2)  # 跳跃误差
+
+    print(round(target_distance), round(jump_error_value), round(actual_distance), round(last_press_time))
+    # 将结果采集进学习字典
+    if last_piece_x > 0 and last_press_time > 0:
+        ai.add_data(round(actual_distance, 2), round(last_press_time))
+        # print(round(actual_distance), round(last_press_time))
 
 def dump_device_info():
-    """
+    '''
     显示设备信息
-    """
+    '''
     size_str = os.popen('adb shell wm size').read()
     device_str = os.popen('adb shell getprop ro.product.device').read()
     phone_os_str = os.popen('adb shell getprop ro.build.version.release').read()
