2252845bd3e79ccd13393071b23c2ea5e068ddac
Willam <mrlevo@outlook.com>
2018-01-04 15:47:51 +0800

    [优化] 使用新的点获取方法，不再依赖配置文件

--- wechat_jump_auto_iOS.py
+++ wechat_jump_auto_iOS.py
@@ -102,52 +102,65 @@ def set_button_position(im):
     top = 1003 * (h / 1280.0) + 10
     swipe_x1, swipe_y1, swipe_x2, swipe_y2 = left, top, left, top
 
-
 def find_piece_and_board(im):
+    """
+    寻找关键坐标
+    """
     w, h = im.size
 
-    print("size: {}, {}".format(w, h))
-
-    piece_x_sum = piece_x_c = piece_y_max = 0
-    board_x = board_y = 0
+    piece_x_sum = 0
+    piece_x_c = 0
+    piece_y_max = 0
+    board_x = 0
+    board_y = 0
     scan_x_border = int(w / 8)  # 扫描棋子时的左右边界
     scan_start_y = 0  # 扫描的起始 y 坐标
     im_pixel = im.load()
-
     # 以 50px 步长，尝试探测 scan_start_y
-    for i in range(under_game_score_y, h, 50):
+    for i in range(int(h / 3), int(h*2 / 3), 50):
         last_pixel = im_pixel[0, i]
         for j in range(1, w):
             pixel = im_pixel[j, i]
-
-            # 不是纯色的线，则记录scan_start_y的值，准备跳出循环
+            # 不是纯色的线，则记录 scan_start_y 的值，准备跳出循环
             if pixel != last_pixel:
                 scan_start_y = i - 50
                 break
-
         if scan_start_y:
             break
+    print('scan_start_y: {}'.format(scan_start_y))
 
-    print("scan_start_y: ", scan_start_y)
+    # 使用像素聚集边界来判断中心
+    # j 横坐标
+    # i 纵坐标
 
-    # 从 scan_start_y 开始往下扫描，棋子应位于屏幕上半部分，这里暂定不超过 2/3
+    pixel_dict_x = dict()
+    pixel_dict_y = dict()
+    # pixel_dict_y = dict()
     for i in range(scan_start_y, int(h * 2 / 3)):
         # 横坐标方面也减少了一部分扫描开销
         for j in range(scan_x_border, w - scan_x_border):
             pixel = im_pixel[j, i]
-            # 根据棋子的最低行的颜色判断，找最后一行那些点的平均值，这个颜
-            # 色这样应该 OK，暂时不提出来
-            if (50 < pixel[0] < 60) \
-                    and (53 < pixel[1] < 63) \
-                    and (95 < pixel[2] < 110):
-                piece_x_sum += j
-                piece_x_c += 1
-                piece_y_max = max(i, piece_y_max)
+            # 根据棋子颜色，进行像素积累记录，棋子最宽的地方即是纵坐标位置，同理可得
+            if (50 < pixel[0] < 60) and (53 < pixel[1] < 63) and (95 < pixel[2] < 110):
+                if j not in pixel_dict_x:
+                    pixel_dict_x[j] = 0
+                pixel_dict_x[j] +=1
+                if i not in pixel_dict_y:
+                    pixel_dict_y[i] = 0
+                pixel_dict_y[i] +=1
 
-    if not all((piece_x_sum, piece_x_c)):
-        return 0, 0, 0, 0
-    piece_x = piece_x_sum / piece_x_c
-    piece_y = piece_y_max - piece_base_height_1_2  # 上移棋子底盘高度的一半
+    piece_x = sorted(pixel_dict_x,key=lambda x:pixel_dict_x[x])[-1]  
+    piece_y = sorted(pixel_dict_y,key=lambda x:pixel_dict_y[x])[-1]
+    # print (piece_x, piece_y)
+
+
+    # 限制棋盘扫描的横坐标，避免音符 bug
+    if piece_x < w/2:
+        board_x_start = piece_x
+        board_x_end = w
+    else:
+        board_x_start = 0
+        board_x_end = piece_x
 
     for i in range(int(h / 3), int(h * 2 / 3)):
         last_pixel = im_pixel[0, i]
@@ -156,11 +169,12 @@ def find_piece_and_board(im):
         board_x_sum = 0
         board_x_c = 0
 
-        for j in range(w):
+        for j in range(int(board_x_start), int(board_x_end)):
             pixel = im_pixel[j, i]
-            # 修掉脑袋比下一个小格子还高的情况的 bug
-            if abs(j - piece_x) < piece_body_width:
-                continue
+
+            # 获取棋子半径
+            if abs(j - piece_x) < pixel_dict_y[sorted(pixel_dict_y,key=lambda x:pixel_dict_y[x])[-1]]:
+                  continue
 
             # 修掉圆顶的时候一条线导致的小 bug，这个颜色判断应该 OK，暂时不提出来
             if abs(pixel[0] - last_pixel[0]) \
@@ -168,17 +182,33 @@ def find_piece_and_board(im):
                     + abs(pixel[2] - last_pixel[2]) > 10:
                 board_x_sum += j
                 board_x_c += 1
-
         if board_x_sum:
             board_x = board_x_sum / board_x_c
+    last_pixel = im_pixel[board_x, i]
 
-    # 按实际的角度来算，找到接近下一个 board 中心的坐标 这里的角度应该
-    # 是 30°,值应该是 tan 30°, math.sqrt(3) / 3
-    board_y = piece_y - abs(board_x - piece_x) * math.sqrt(3) / 3
+    # 从上顶点往下 +274 的位置开始向上找颜色与上顶点一样的点，为下顶点
+    # 该方法对所有纯色平面和部分非纯色平面有效，对高尔夫草坪面、木纹桌面、
+    # 药瓶和非菱形的碟机（好像是）会判断错误
+    for k in range(i+274, i, -1):  # 274 取开局时最大的方块的上下顶点距离
+        pixel = im_pixel[board_x, k]
+        if abs(pixel[0] - last_pixel[0]) \
+                + abs(pixel[1] - last_pixel[1]) \
+                + abs(pixel[2] - last_pixel[2]) < 10:
+            break
+    board_y = int((i+k) / 2)
+
+    # 如果上一跳命中中间，则下个目标中心会出现 r245 g245 b245 的点，利用这个
+    # 属性弥补上一段代码可能存在的判断错误
+    # 若上一跳由于某种原因没有跳到正中间，而下一跳恰好有无法正确识别花纹，则有
+    # 可能游戏失败，由于花纹面积通常比较大，失败概率较低
+    for j in range(i, i+200):
+        pixel = im_pixel[board_x, j]
+        if abs(pixel[0] - 245) + abs(pixel[1] - 245) + abs(pixel[2] - 245) == 0:
+            board_y = j + 10
+            break
 
     if not all((board_x, board_y)):
         return 0, 0, 0, 0
-
     return piece_x, piece_y, board_x, board_y
 
 
