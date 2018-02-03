388935685fb32d61a1774e131401c3c138cba3cb
柴茂源 <945852046@qq.com>
2018-02-03 09:13:03 +0800

    [优化] windows下cmd乱码处理，同时兼容linux，By @ameizi (#1169)

---
+++ common/UnicodeStreamFilter.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import sys
+
+
+class UnicodeStreamFilter:
+    def __init__(self, target):
+        self.target = target
+        self.encoding = 'utf-8'
+        self.errors = 'replace'
+        self.encode_to = self.target.encoding
+
+    def write(self, s):
+        if type(s) == str:
+            s = s.decode("utf-8")
+        s = s.encode(self.encode_to, self.errors).decode(self.encode_to)
+        self.target.write(s)
+
+
+if sys.stdout.encoding == 'cp936':
+    sys.stdout = UnicodeStreamFilter(sys.stdout)
