479c95e20139b3dca840a22caa77943c52bc57a9
leon332157 <leon332157@sina.com>
2018-02-13 13:20:20 -0500

    Update debug.py

--- common/debug.py
+++ common/debug.py
@@ -54,7 +54,7 @@ def save_debug_screenshot(ts, im, piece_x, piece_y, board_x, board_y, debugtype=
     draw.ellipse((piece_x - 10, piece_y - 10, piece_x + 10, piece_y + 10), fill=(255, 0, 0))
     draw.ellipse((board_x - 10, board_y - 10, board_x + 10, board_y + 10), fill=(0, 0, 255))
     del draw
-	if platform.system=='Windows':
+	if platform.system()=='Windows':
         im.save('{}\{} {}_{}.png'.format(screenshot_backup_dir, time.strftime('%y-%m-%d %H:%M:%S', time.localtime()), ts,debugtype))
     else:
 	    im.save('{}/{} {}_{}.png'.format(screenshot_backup_dir, time.strftime('%y-%m-%d %H:%M:%S', time.localtime()), ts,debugtype))
