7ce85dc2bc237973c96382ad11df833f45daf8b9
leon332157 <leon332157@sina.com>
2018-02-23 06:28:46 -0500

    [修复] 编码及算法 bug (#1208)
    * [修复] 修复 #1200 增加__init__.py unicode增加版本识别
    
    * [修复] 修复 bug and PEP8
    
    * [修复] 修改长按时间系数（press_coefficient）无效
    
    press_coefficient 并未用于计算 press_time。
    
    * [README] 添加统计图 :joy: (#1207)

--- common/UnicodeStreamFilter.py
+++ common/UnicodeStreamFilter.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import sys
 
+if sys.version_info.major != 3:
+    class UnicodeStreamFilter:
 
-class UnicodeStreamFilter:
-    def __init__(self, target):
-        self.target = target
-        self.encoding = 'utf-8'
-        self.errors = 'replace'
-        self.encode_to = self.target.encoding
+        def __init__(self, target):
+            self.target = target
+            self.encoding = 'utf-8'
+            self.errors = 'replace'
+            self.encode_to = self.target.encoding
+
 
     def write(self, s):
         if type(s) == str:
@@ -16,5 +18,7 @@ class UnicodeStreamFilter:
         self.target.write(s)
 
 
-if sys.stdout.encoding == 'cp936':
-    sys.stdout = UnicodeStreamFilter(sys.stdout)
+    if sys.stdout.encoding == 'cp936':
+        sys.stdout = UnicodeStreamFilter(sys.stdout)
+else:
+    pass
